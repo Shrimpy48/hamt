d7c1dc6ddacd74c4755193fb39de85c0d6ddc783
Shrimpy48 <45856863+Shrimpy48@users.noreply.github.com>
2023-07-15 19:14:39 +0100

    Move node logic to own file

---
+++ src/simple/mod.rs
@@ -0,0 +1,376 @@
+use std::borrow::Borrow;
+use std::collections::hash_map::RandomState;
+use std::hash::{BuildHasher, Hash};
+use std::ops::Index;
+use std::rc::Rc;
+use std::{fmt, iter, mem};
+
+use crate::hash_seq::HashSeq;
+
+mod node;
+use node::*;
+
+pub struct Hamt<K, V, S = RandomState> {
+    root: IntNode<K, V>,
+    hash_builder: S,
+    size: usize,
+}
+
+impl<K, V, S> Hamt<K, V, S>
+where
+    S: Default,
+{
+    pub fn new() -> Self {
+        Self::with_hasher(S::default())
+    }
+}
+
+impl<K, V, S> Hamt<K, V, S> {
+    pub fn with_hasher(hash_builder: S) -> Self {
+        Self {
+            root: IntNode::new(),
+            hash_builder,
+            size: 0,
+        }
+    }
+
+    pub fn iter(&self) -> Iter<'_, K, V> {
+        Iter::new(self)
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
+
+impl<K, V, S> Hamt<K, V, S>
+where
+    K: Eq + Hash,
+    S: BuildHasher,
+{
+    pub fn get_key_value<Q>(&self, k: &Q) -> Option<(&K, &V)>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        let hash_seq = HashSeq::new(k, &self.hash_builder);
+        self.root
+            .search(hash_seq)
+            .map(|leaf| (leaf.key(), leaf.value()))
+    }
+
+    pub fn get<Q>(&self, k: &Q) -> Option<&V>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        self.get_key_value(k).map(|(_, v)| v)
+    }
+}
+
+impl<K, V, S> Hamt<K, V, S>
+where
+    K: Eq + Hash + Clone,
+    V: Clone,
+    S: BuildHasher,
+{
+    pub fn get_mut<Q>(&mut self, k: &Q) -> Option<&mut V>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        let mut hash_seq = HashSeq::new(k, &self.hash_builder);
+        let result = self.root.search_mut(&mut hash_seq);
+        match result.kind {
+            SearchResultKind::Success => {
+                let Some(Node::Leaf(leaf)) = result.anchor.get_mut(result.index) else { unreachable!() };
+                Some(leaf.value_mut())
+            }
+            _ => None,
+        }
+    }
+
+    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+        let key = Rc::new(k);
+        let hs_key = Rc::clone(&key);
+        let mut hash_seq = HashSeq::new(&*hs_key, &self.hash_builder);
+        let result = self.root.search_mut(&mut hash_seq);
+        match result.kind {
+            SearchResultKind::Success => {
+                let Some(Node::Leaf(leaf)) = result.anchor.get_mut(result.index) else { unreachable!() };
+                Some(mem::replace(leaf.value_mut(), v))
+            }
+            SearchResultKind::NotFound => {
+                *result.anchor = result
+                    .anchor
+                    .insert(result.index, Node::Leaf(LeafNode::new(key, Rc::new(v))));
+                self.size += 1;
+                None
+            }
+            SearchResultKind::Mismatch => {
+                let Some(Node::Leaf(leaf)) = result.anchor.get(result.index) else { unreachable!() };
+                let leaf = leaf.clone();
+                let other_hash_seq = hash_seq.with_key(leaf.key());
+                let paired_seq = hash_seq.zip(other_hash_seq);
+                let Some(child) = result.anchor.get_mut(result.index) else { unreachable!() };
+                *child = Node::Int(IntNode::make_table(
+                    paired_seq,
+                    LeafNode::new(key, Rc::new(v)),
+                    leaf.clone(),
+                ));
+                self.size += 1;
+                None
+            }
+        }
+    }
+
+    pub fn remove_entry<Q>(&mut self, k: &Q) -> Option<(K, V)>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        let hash_seq = HashSeq::new(k, &self.hash_builder);
+        let result = self.root.remove(hash_seq);
+        match result {
+            RemoveResult::Success { key, value } | RemoveResult::Gathered { key, value } => {
+                self.size -= 1;
+                Some((key, value))
+            }
+            RemoveResult::NotFound => None,
+        }
+    }
+
+    pub fn remove<Q>(&mut self, k: &Q) -> Option<V>
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        self.remove_entry(k).map(|(_, v)| v)
+    }
+}
+
+impl<K, V, S> Hamt<K, V, S>
+where
+    K: Eq + Hash + Clone,
+    V: Clone,
+    S: BuildHasher + Clone,
+{
+    pub fn insert_immut(&self, k: K, v: V) -> Self {
+        // Shallow copy
+        let mut out = self.clone();
+        out.insert(k, v);
+        out
+    }
+
+    pub fn remove_immut<Q>(&self, k: &Q) -> Self
+    where
+        K: Borrow<Q>,
+        Q: Hash + Eq + ?Sized,
+    {
+        // Shallow copy
+        let mut out = self.clone();
+        out.remove(k);
+        out
+    }
+}
+
+impl<K, V, S> Default for Hamt<K, V, S>
+where
+    S: Default,
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<K, V, S: Clone> Clone for Hamt<K, V, S> {
+    fn clone(&self) -> Self {
+        Self {
+            root: self.root.clone(),
+            hash_builder: self.hash_builder.clone(),
+            size: self.size,
+        }
+    }
+}
+
+impl<K, V, S> Extend<(K, V)> for Hamt<K, V, S>
+where
+    K: Eq + Hash + Clone,
+    V: Clone,
+    S: BuildHasher,
+{
+    fn extend<T: IntoIterator<Item = (K, V)>>(&mut self, iter: T) {
+        for (key, value) in iter {
+            self.insert(key, value);
+        }
+    }
+}
+
+impl<'a, K, V, S> Extend<(&'a K, &'a V)> for Hamt<K, V, S>
+where
+    K: Eq + Hash + Copy,
+    V: Copy,
+    S: BuildHasher,
+{
+    fn extend<T: IntoIterator<Item = (&'a K, &'a V)>>(&mut self, iter: T) {
+        for (key, value) in iter {
+            self.insert(*key, *value);
+        }
+    }
+}
+
+impl<K, V, S> FromIterator<(K, V)> for Hamt<K, V, S>
+where
+    K: Eq + Hash + Clone,
+    V: Clone,
+    S: BuildHasher + Default,
+{
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+        let mut out = Self::default();
+        for (key, value) in iter {
+            out.insert(key, value);
+        }
+        out
+    }
+}
+
+impl<'a, K, V, S> IntoIterator for &'a Hamt<K, V, S> {
+    type IntoIter = Iter<'a, K, V>;
+    type Item = (&'a K, &'a V);
+
+    #[inline]
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+// Constraining S is more ergonomic for creating map "literals" - see the note on HashMap::from.
+impl<K, V, const N: usize> From<[(K, V); N]> for Hamt<K, V, RandomState>
+where
+    K: Eq + Hash + Clone,
+    V: Clone,
+{
+    fn from(arr: [(K, V); N]) -> Self {
+        Self::from_iter(arr)
+    }
+}
+
+impl<K, V, S> PartialEq for Hamt<K, V, S>
+where
+    K: Eq + Hash,
+    V: PartialEq,
+    S: BuildHasher,
+{
+    fn eq(&self, other: &Hamt<K, V, S>) -> bool {
+        if self.len() != other.len() {
+            return false;
+        }
+
+        self.iter()
+            .all(|(key, value)| other.get(key).map_or(false, |v| *value == *v))
+    }
+}
+
+impl<K, V, S> Eq for Hamt<K, V, S>
+where
+    K: Eq + Hash,
+    V: Eq,
+    S: BuildHasher,
+{
+}
+
+impl<K, Q, V, S> Index<&Q> for Hamt<K, V, S>
+where
+    K: Eq + Hash + Borrow<Q>,
+    Q: Eq + Hash + ?Sized,
+    S: BuildHasher,
+{
+    type Output = V;
+
+    #[inline]
+    fn index(&self, index: &Q) -> &Self::Output {
+        self.get(index).expect("no entry found for key")
+    }
+}
+
+impl<K: fmt::Debug, V: fmt::Debug, S> fmt::Debug for Hamt<K, V, S> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut map = f.debug_map();
+        map.entries(self.iter());
+        map.finish()
+    }
+}
+
+pub struct Iter<'a, K, V> {
+    stack: Vec<IterItem<'a, K, V>>,
+    len: usize,
+}
+
+struct IterItem<'a, K, V> {
+    node: &'a IntNode<K, V>,
+    child: usize,
+}
+
+impl<'a, K, V> IterItem<'a, K, V> {
+    fn new(node: &'a IntNode<K, V>) -> Self {
+        Self { node, child: 0 }
+    }
+}
+
+impl<'a, K, V> Iter<'a, K, V> {
+    fn new<S>(hamt: &'a Hamt<K, V, S>) -> Self {
+        let exp_depth = hamt
+            .size
+            .checked_ilog(64)
+            .map(|d| d as usize + 2)
+            .unwrap_or(1);
+        let mut stack = Vec::with_capacity(exp_depth);
+        stack.push(IterItem::new(&hamt.root));
+        Self {
+            stack,
+            len: hamt.size,
+        }
+    }
+}
+
+impl<'a, K, V> Iterator for Iter<'a, K, V> {
+    type Item = (&'a K, &'a V);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.stack.last_mut() {
+                Some(item) => match item.node.nth_child(item.child) {
+                    Some(Node::Int(int)) => self.stack.push(IterItem::new(int)),
+                    Some(Node::Leaf(leaf)) => {
+                        let res = Some((leaf.key(), leaf.value()));
+                        item.child += 1;
+                        self.len -= 1;
+                        break res;
+                    }
+                    None => {
+                        self.stack.pop();
+                        if let Some(item) = self.stack.last_mut() {
+                            item.child += 1
+                        }
+                    }
+                },
+                None => {
+                    break None;
+                }
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+impl<'a, K, V> iter::FusedIterator for Iter<'a, K, V> {}
+
+impl<'a, K, V> ExactSizeIterator for Iter<'a, K, V> {}
--- src/simple/node.rs
+++ src/simple/node.rs
@@ -1,330 +1,47 @@
 use std::borrow::Borrow;
 use std::cmp::Ordering;
-use std::collections::hash_map::RandomState;
+use std::fmt;
 use std::hash::{BuildHasher, Hash};
 use std::ops::{Index, IndexMut};
 use std::rc::Rc;
-use std::{fmt, iter, mem};
 
 use crate::hash_seq::HashSeq;
 
-pub struct Hamt<K, V, S = RandomState> {
-    root: IntNode<K, V>,
-    hash_builder: S,
-    size: usize,
+#[derive(Debug, Clone, Copy)]
+pub enum SearchResultKind {
+    Success,
+    Mismatch,
+    NotFound,
 }
 
-impl<K, V, S> Hamt<K, V, S>
-where
-    S: Default,
-{
-    pub fn new() -> Self {
-        Self::with_hasher(S::default())
-    }
+#[derive(Debug)]
+pub struct SearchMutResult<'a, K, V> {
+    pub kind: SearchResultKind,
+    pub anchor: &'a mut IntNode<K, V>,
+    pub index: usize,
 }
 
-impl<K, V, S> Hamt<K, V, S> {
-    pub fn with_hasher(hash_builder: S) -> Self {
-        Self {
-            root: IntNode::new(),
-            hash_builder,
-            size: 0,
-        }
-    }
-
-    pub fn iter(&self) -> Iter<'_, K, V> {
-        Iter::new(self)
-    }
-
-    pub fn len(&self) -> usize {
-        self.size
-    }
-
-    #[inline]
-    pub fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
+#[derive(Debug)]
+pub enum RemoveResult<K, V> {
+    Success { key: K, value: V },
+    Gathered { key: K, value: V },
+    NotFound,
 }
 
-impl<K, V, S> Hamt<K, V, S>
-where
-    K: Eq + Hash,
-    S: BuildHasher,
-{
-    pub fn get_key_value<Q>(&self, k: &Q) -> Option<(&K, &V)>
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized,
-    {
-        let hash_seq = HashSeq::new(k, &self.hash_builder);
-        self.root
-            .search(hash_seq)
-            .map(|leaf| (&*leaf.key, &*leaf.value))
-    }
-
-    pub fn get<Q>(&self, k: &Q) -> Option<&V>
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized,
-    {
-        self.get_key_value(k).map(|(_, v)| v)
-    }
-}
-
-impl<K, V, S> Hamt<K, V, S>
-where
-    K: Eq + Hash + Clone,
-    V: Clone,
-    S: BuildHasher,
-{
-    pub fn get_mut<Q>(&mut self, k: &Q) -> Option<&mut V>
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized,
-    {
-        let mut hash_seq = HashSeq::new(k, &self.hash_builder);
-        let result = self.root.search_mut(&mut hash_seq);
-        match result.kind {
-            SearchResultKind::Success => {
-                let Some(Node::Leaf(leaf)) = result.anchor.get_mut(result.index) else { unreachable!() };
-                Some(Rc::make_mut(&mut leaf.value))
-            }
-            _ => None,
-        }
-    }
-
-    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
-        let key = Rc::new(k);
-        let hs_key = Rc::clone(&key);
-        let mut hash_seq = HashSeq::new(&*hs_key, &self.hash_builder);
-        let result = self.root.search_mut(&mut hash_seq);
-        match result.kind {
-            SearchResultKind::Success => {
-                let Some(Node::Leaf(leaf)) = result.anchor.get_mut(result.index) else { unreachable!() };
-                Some(mem::replace(Rc::make_mut(&mut leaf.value), v))
-            }
-            SearchResultKind::NotFound => {
-                *result.anchor = result.anchor.insert(
-                    result.index,
-                    Node::Leaf(LeafNode {
-                        key,
-                        value: Rc::new(v),
-                    }),
-                );
-                self.size += 1;
-                None
-            }
-            SearchResultKind::Mismatch => {
-                let Some(Node::Leaf(leaf)) = result.anchor.get(result.index) else { unreachable!() };
-                let leaf = leaf.clone();
-                let other_hash_seq = hash_seq.with_key(&leaf.key);
-                let paired_seq = hash_seq.zip(other_hash_seq);
-                let Some(child) = result.anchor.get_mut(result.index) else { unreachable!() };
-                *child = Node::Int(IntNode::make_table(
-                    paired_seq,
-                    LeafNode {
-                        key,
-                        value: Rc::new(v),
-                    },
-                    leaf.clone(),
-                ));
-                self.size += 1;
-                None
-            }
-        }
-    }
-
-    pub fn remove_entry<Q>(&mut self, k: &Q) -> Option<(K, V)>
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized,
-    {
-        let hash_seq = HashSeq::new(k, &self.hash_builder);
-        let result = self.root.remove(hash_seq);
-        match result {
-            RemoveResult::Success { key, value } | RemoveResult::Gathered { key, value } => {
-                self.size -= 1;
-                Some((key, value))
-            }
-            RemoveResult::NotFound => None,
-        }
-    }
-
-    pub fn remove<Q>(&mut self, k: &Q) -> Option<V>
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized,
-    {
-        self.remove_entry(k).map(|(_, v)| v)
-    }
-}
-
-impl<K, V, S> Hamt<K, V, S>
-where
-    K: Eq + Hash + Clone,
-    V: Clone,
-    S: BuildHasher + Clone,
-{
-    pub fn insert_immut(&self, k: K, v: V) -> Self {
-        // Shallow copy
-        let mut out = self.clone();
-        out.insert(k, v);
-        out
-    }
-
-    pub fn remove_immut<Q>(&self, k: &Q) -> Self
-    where
-        K: Borrow<Q>,
-        Q: Hash + Eq + ?Sized,
-    {
-        // Shallow copy
-        let mut out = self.clone();
-        out.remove(k);
-        out
-    }
-}
-
-impl<K, V, S> Default for Hamt<K, V, S>
-where
-    S: Default,
-{
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<K, V, S: Clone> Clone for Hamt<K, V, S> {
-    fn clone(&self) -> Self {
-        Self {
-            root: self.root.clone(),
-            hash_builder: self.hash_builder.clone(),
-            size: self.size,
-        }
-    }
-}
-
-impl<K, V, S> Extend<(K, V)> for Hamt<K, V, S>
-where
-    K: Eq + Hash + Clone,
-    V: Clone,
-    S: BuildHasher,
-{
-    fn extend<T: IntoIterator<Item = (K, V)>>(&mut self, iter: T) {
-        for (key, value) in iter {
-            self.insert(key, value);
-        }
-    }
-}
-
-impl<'a, K, V, S> Extend<(&'a K, &'a V)> for Hamt<K, V, S>
-where
-    K: Eq + Hash + Copy,
-    V: Copy,
-    S: BuildHasher,
-{
-    fn extend<T: IntoIterator<Item = (&'a K, &'a V)>>(&mut self, iter: T) {
-        for (key, value) in iter {
-            self.insert(*key, *value);
-        }
-    }
-}
-
-impl<K, V, S> FromIterator<(K, V)> for Hamt<K, V, S>
-where
-    K: Eq + Hash + Clone,
-    V: Clone,
-    S: BuildHasher + Default,
-{
-    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
-        let mut out = Self::default();
-        for (key, value) in iter {
-            out.insert(key, value);
-        }
-        out
-    }
-}
-
-impl<'a, K, V, S> IntoIterator for &'a Hamt<K, V, S> {
-    type IntoIter = Iter<'a, K, V>;
-    type Item = (&'a K, &'a V);
-
-    #[inline]
-    fn into_iter(self) -> Self::IntoIter {
-        self.iter()
-    }
-}
-
-// Constraining S is more ergonomic for creating map "literals" - see the note on HashMap::from.
-impl<K, V, const N: usize> From<[(K, V); N]> for Hamt<K, V, RandomState>
-where
-    K: Eq + Hash + Clone,
-    V: Clone,
-{
-    fn from(arr: [(K, V); N]) -> Self {
-        Self::from_iter(arr)
-    }
-}
-
-impl<K, V, S> PartialEq for Hamt<K, V, S>
-where
-    K: Eq + Hash,
-    V: PartialEq,
-    S: BuildHasher,
-{
-    fn eq(&self, other: &Hamt<K, V, S>) -> bool {
-        if self.len() != other.len() {
-            return false;
-        }
-
-        self.iter()
-            .all(|(key, value)| other.get(key).map_or(false, |v| *value == *v))
-    }
-}
-
-impl<K, V, S> Eq for Hamt<K, V, S>
-where
-    K: Eq + Hash,
-    V: Eq,
-    S: BuildHasher,
-{
-}
-
-impl<K, Q, V, S> Index<&Q> for Hamt<K, V, S>
-where
-    K: Eq + Hash + Borrow<Q>,
-    Q: Eq + Hash + ?Sized,
-    S: BuildHasher,
-{
-    type Output = V;
-
-    #[inline]
-    fn index(&self, index: &Q) -> &Self::Output {
-        self.get(index).expect("no entry found for key")
-    }
-}
-
-impl<K: fmt::Debug, V: fmt::Debug, S> fmt::Debug for Hamt<K, V, S> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut map = f.debug_map();
-        map.entries(self.iter());
-        map.finish()
-    }
-}
-
-struct IntNode<K, V> {
+pub struct IntNode<K, V> {
     bitmap: u64,
     children: Rc<[Node<K, V>]>,
 }
 
 impl<K, V> IntNode<K, V> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Self {
             bitmap: 0,
             children: Rc::new([]),
         }
     }
 
-    fn get(&self, index: usize) -> Option<&Node<K, V>> {
+    pub fn get(&self, index: usize) -> Option<&Node<K, V>> {
         if self.bitmap & (1 << index) != 0 {
             let dense_idx = (self.bitmap & ((1 << index) - 1)).count_ones();
             Some(self.children.index(dense_idx as usize))
@@ -333,7 +50,7 @@ impl<K, V> IntNode<K, V> {
         }
     }
 
-    fn get_mut(&mut self, index: usize) -> Option<&mut Node<K, V>> {
+    pub fn get_mut(&mut self, index: usize) -> Option<&mut Node<K, V>> {
         if self.bitmap & (1 << index) != 0 {
             let dense_idx = (self.bitmap & ((1 << index) - 1)).count_ones();
             Some(make_mut_unsized(&mut self.children).index_mut(dense_idx as usize))
@@ -342,7 +59,11 @@ impl<K, V> IntNode<K, V> {
         }
     }
 
-    fn insert(&self, index: usize, child: Node<K, V>) -> Self {
+    pub fn nth_child(&self, n: usize) -> Option<&Node<K, V>> {
+        self.children.get(n)
+    }
+
+    pub fn insert(&self, index: usize, child: Node<K, V>) -> Self {
         let bitmap = self.bitmap | (1 << index);
         debug_assert_ne!(
             bitmap, self.bitmap,
@@ -363,7 +84,7 @@ impl<K, V> IntNode<K, V> {
         res
     }
 
-    fn make_table(
+    pub fn make_table(
         mut paired_seq: impl Iterator<Item = (usize, usize)>,
         left_leaf: LeafNode<K, V>,
         right_leaf: LeafNode<K, V>,
@@ -411,7 +132,7 @@ where
     V: Clone,
 {
     // Remove without gathering this node.
-    fn remove<Q, S>(&mut self, mut hash_seq: HashSeq<'_, Q, S>) -> RemoveResult<K, V>
+    pub fn remove<Q, S>(&mut self, mut hash_seq: HashSeq<'_, Q, S>) -> RemoveResult<K, V>
     where
         K: Borrow<Q>,
         Q: Hash + Eq + ?Sized,
@@ -470,7 +191,7 @@ impl<K, V> IntNode<K, V>
 where
     K: Eq + Hash,
 {
-    fn search<'a, Q, S>(&'a self, mut hash_seq: HashSeq<'_, Q, S>) -> Option<&'a LeafNode<K, V>>
+    pub fn search<'a, Q, S>(&'a self, mut hash_seq: HashSeq<'_, Q, S>) -> Option<&'a LeafNode<K, V>>
     where
         K: Borrow<Q>,
         Q: Hash + Eq + ?Sized,
@@ -490,7 +211,7 @@ where
         }
     }
 
-    fn search_mut<'a, Q, S>(
+    pub fn search_mut<'a, Q, S>(
         &'a mut self,
         hash_seq: &mut HashSeq<'_, Q, S>,
     ) -> SearchMutResult<'a, K, V>
@@ -540,9 +261,29 @@ impl<K, V> Clone for IntNode<K, V> {
 }
 
 #[derive(Debug)]
-struct LeafNode<K, V> {
+pub struct LeafNode<K, V> {
     key: Rc<K>,
     value: Rc<V>,
+}
+
+impl<K, V> LeafNode<K, V> {
+    pub fn new(key: Rc<K>, value: Rc<V>) -> Self {
+        Self { key, value }
+    }
+
+    pub fn key(&self) -> &K {
+        &self.key
+    }
+
+    pub fn value(&self) -> &V {
+        &self.value
+    }
+}
+
+impl<K, V: Clone> LeafNode<K, V> {
+    pub fn value_mut(&mut self) -> &mut V {
+        Rc::make_mut(&mut self.value)
+    }
 }
 
 impl<K, V> Clone for LeafNode<K, V> {
@@ -555,7 +296,7 @@ impl<K, V> Clone for LeafNode<K, V> {
 }
 
 #[derive(Debug)]
-enum Node<K, V> {
+pub enum Node<K, V> {
     Int(IntNode<K, V>),
     Leaf(LeafNode<K, V>),
 }
@@ -635,93 +376,3 @@ impl<K, V> Clone for Node<K, V> {
         }
     }
 }
-
-#[derive(Debug, Clone, Copy)]
-enum SearchResultKind {
-    Success,
-    Mismatch,
-    NotFound,
-}
-
-#[derive(Debug)]
-struct SearchMutResult<'a, K, V> {
-    kind: SearchResultKind,
-    anchor: &'a mut IntNode<K, V>,
-    index: usize,
-}
-
-#[derive(Debug)]
-enum RemoveResult<K, V> {
-    Success { key: K, value: V },
-    Gathered { key: K, value: V },
-    NotFound,
-}
-
-pub struct Iter<'a, K, V> {
-    stack: Vec<IterItem<'a, K, V>>,
-    len: usize,
-}
-
-struct IterItem<'a, K, V> {
-    node: &'a IntNode<K, V>,
-    child: usize,
-}
-
-impl<'a, K, V> IterItem<'a, K, V> {
-    fn new(node: &'a IntNode<K, V>) -> Self {
-        Self { node, child: 0 }
-    }
-}
-
-impl<'a, K, V> Iter<'a, K, V> {
-    fn new<S>(hamt: &'a Hamt<K, V, S>) -> Self {
-        let exp_depth = hamt
-            .size
-            .checked_ilog(64)
-            .map(|d| d as usize + 2)
-            .unwrap_or(1);
-        let mut stack = Vec::with_capacity(exp_depth);
-        stack.push(IterItem::new(&hamt.root));
-        Self {
-            stack,
-            len: hamt.size,
-        }
-    }
-}
-
-impl<'a, K, V> Iterator for Iter<'a, K, V> {
-    type Item = (&'a K, &'a V);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            match self.stack.last_mut() {
-                Some(item) => match item.node.children.get(item.child) {
-                    Some(Node::Int(int)) => self.stack.push(IterItem::new(int)),
-                    Some(Node::Leaf(leaf)) => {
-                        let res = Some((&*leaf.key, &*leaf.value));
-                        item.child += 1;
-                        self.len -= 1;
-                        break res;
-                    }
-                    None => {
-                        self.stack.pop();
-                        if let Some(item) = self.stack.last_mut() {
-                            item.child += 1
-                        }
-                    }
-                },
-                None => {
-                    break None;
-                }
-            }
-        }
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len, Some(self.len))
-    }
-}
-
-impl<'a, K, V> iter::FusedIterator for Iter<'a, K, V> {}
-
-impl<'a, K, V> ExactSizeIterator for Iter<'a, K, V> {}
