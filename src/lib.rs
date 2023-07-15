e6da77c1e9bc03934b72138a7b2b0b4a4298e584
Shrimpy48 <45856863+Shrimpy48@users.noreply.github.com>
2023-07-15 15:47:40 +0100

    Add property testing

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,35 @@ mod simple;
 
 pub use simple::Hamt;
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
+    use proptest::prelude::*;
+    use proptest_derive::Arbitrary;
+
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[derive(Debug, Arbitrary)]
+    enum Op<K, V> {
+        Insert { key: K, value: V },
+        Get { key: K },
+        Remove { key: K },
+    }
+
+    proptest! {
+        #[test]
+        fn matches_hashmap(ops in prop::collection::vec(any::<Op<u8, u8>>(), prop::collection::SizeRange::default())) {
+            let mut hamt: Hamt<u8, u8> = Hamt::new();
+            let mut hashmap: HashMap<u8, u8> = HashMap::new();
+
+            for op in ops {
+                match op {
+                    Op::Insert { key, value } => prop_assert_eq!(hamt.insert(key, value), hashmap.insert(key, value)),
+                    Op::Get { key } => prop_assert_eq!(hamt.get(&key), hashmap.get(&key)),
+                    Op::Remove { key } => prop_assert_eq!(hamt.remove(&key), hashmap.remove(&key)),
+                }
+            }
+        }
     }
 }
